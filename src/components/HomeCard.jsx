04c6b67fc027fc823ed937b397f46242e49a878a
petter <petter0915@gmail.com>
2022-01-13 21:12:21 -0500

    Ajustes a tarjetas de homecard

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,26 +1,22 @@
 import React from "react"
-import { Card, CardGroup} from "react-bootstrap"
+// import { Card, CardGroup} from "react-bootstrap"
 import "../style/CardStyle.css"
 
 // eslint-disable-next-line react/prop-types
 export const HomeCard = ({ title, description, image }) => {
   return (
-    <>
-      <CardGroup>
-        <Card className="cardProduct">
-          <Card.Img className="image" variant="bottom" src={`${image}`} />
-          <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>
+        <div className="cardProduct">
+          <img className="image" variant="bottom" src={`${image}`} />
+          <div>
+            <h2>{title}</h2>
+            <p>
             {description}
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
+            </p>
+          </div>
+          <div>
             <small className="text-muted"> Espacio para cronometro</small>
-          </Card.Footer>
-        </Card>   
-    
-      </CardGroup>
-    </>
+          </div>
+        </div>   
+  
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react"
 import { HomeCard } from "../components/HomeCard"
+import "../style/Home.css"
 
 export const Home = () => {
   const [users, setUsers] = useState([])
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products?limit=6")
+    fetch("https://fakestoreapi.com/products?limit=12")
       .then((response) => response.json())
       .then((data) => {
         setUsers(data)
@@ -17,15 +18,16 @@ export const Home = () => {
       <div className="header">
         <h1 className="title">About Products</h1>
       </div>
-
-      {users.map(({ id, title, description, image }) => (
-        <HomeCard
-          key={id}
-          title={title}
-          description={description}
-          image={image}
-        />
-      ))}
+      <div className="container">
+        {users.map(({ id, title, description, image }) => (
+          <HomeCard
+            key={id}
+            title={title}
+            description={description}
+            image={image}
+          />
+        ))}
+      </div>
     </>
   )
 }
